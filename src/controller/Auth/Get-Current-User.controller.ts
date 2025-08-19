be842d4a6052c5c01a70081efbd698028d0c7ce7
25LP4381 25LP4381 <25LP4381@pinecones-iMac.local>
2025-08-19 09:42:18 +0800

    sigin-in

---
+++ src/controller/Auth/Get-Current-User.controller.ts
@@ -0,0 +1,32 @@
+import { Response } from "express";
+import { prisma } from "../../utils/prisma";
+import { GetUserAuthInfoRequest } from "../../middleware/jwt-verify";
+
+export const getCurrentUser = async (
+  req: GetUserAuthInfoRequest,
+  res: Response
+) => {
+  try {
+
+    console.log("req: user", req.user);
+    
+    const headerUser = req.user;
+
+    if (!headerUser) {
+      res.status(400).json({ message: "Error" });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: headerUser?.userId },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: "No user info in request" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error :"error get current user"});
+  }
+};
--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { createUser } from "../controller/Auth/Sign-Up.controller";
+import { authenticateToken } from "../middleware/jwt-verify";
+import { getCurrentUser } from "../controller/Auth/Get-Current-User.controller";
+import { SignIn } from "../controller/Auth/Sign-in.controller";
 
 const AuthRouter = express.Router();
 
 AuthRouter.post("/sign-up", createUser);
+AuthRouter.post("/sign-in", SignIn);
+AuthRouter.get("/get-current-user", authenticateToken, getCurrentUser);
+
 
 
 export default AuthRouter;
